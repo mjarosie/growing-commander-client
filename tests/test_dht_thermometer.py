a1c2e9e7279e9749212088f47c02f49c9fb97f6d
mjarosie <jarosiewicz.maciek@gmail.com>
2017-12-28 22:03:21 +0000

    Added a few more tests.

--- tests/test_dht_thermometer.py
+++ tests/test_dht_thermometer.py
@@ -1,8 +1,9 @@
 from source.dht_thermometer import DhtThermometer
-
+from source.utils import get_formatted_measurement
 import unittest
 import Adafruit_DHT
-
+import pandas as pd
+from datetime import datetime
 
 class TestDhtThermometer(unittest.TestCase):
 
@@ -11,26 +12,39 @@ class TestDhtThermometer(unittest.TestCase):
 		self.assertEqual(thermometer.gpio_pin, 4)
 		self.assertEqual(thermometer.sensor_type, Adafruit_DHT.AM2302)
 
-	def test_get_reading(self):
+	def test_get_measurement(self):
 		'''If fails - probably the thermometer is not connected properly'''
 		thermometer = DhtThermometer('Thermometer 1', 4)
+		def measure_function():
+			return (25.5, 30.0)
+			
+		results = thermometer.get_measurement(measure_function)
 		
-		reading = thermometer.get_reading()
+		self.assertEqual(results['device_name'][0], 'Thermometer 1')
+		self.assertEqual(results['device_name'][1], 'Thermometer 1')
 		
-		self.assertIsInstance(reading[0], (int, long, float))
-		self.assertIsInstance(reading[1], (int, long, float))
+		self.assertEqual(results['measurement_type'][0], 'humidity')
+		self.assertEqual(results['measurement_type'][1], 'temperature')
 		
-	def test_get_reading(self):
-		thermometer = DhtThermometer('Thermometer 1', 4)
+		self.assertEqual(results['measurement_unit'][0], '%')
+		self.assertEqual(results['measurement_unit'][1], '*C')
 		
-		formatted_measurement = thermometer.get_formatted_measurement((26.7, 33.4))
+		self.assertEqual(results['measurement_value'][0], 25.5)
+		self.assertEqual(results['measurement_value'][1], 30.0)
+		
+	def test_get_formated_measurement(self):
+		timestamp = datetime(2017, 12, 20, 21, 30, 50)
+		
+		data = [timestamp, 'Device 1', 'temperature', '*C', 20.0]
+		
+		df = pd.Series(data, index=['timestamp', 'device_name', 'measurement_type', 'measurement_unit', 'measurement_value'])
+		
+		formatted_measurement = get_formatted_measurement(df)
 		self.assertIsInstance(formatted_measurement, str)
-		self.assertEqual(formatted_measurement, "Temp=26.7*  Humidity=33.4%")
+		self.assertEqual(formatted_measurement, '20 Dec 2017, 21:30:50 - Device 1: temperature=20.0*C')
 		
 	def test_get_reading_no_values_throws_exception(self):
-		thermometer = DhtThermometer('Thermometer 1', 4)
-		
-		self.assertRaises(ValueError, thermometer.get_formatted_measurement, (26.7,))
+		self.assertRaises(TypeError, get_formatted_measurement, (26.7,))
 
 if __name__ == '__main__':
 	unittest.main()
--- tests/test_measurement_reader.py
+++ tests/test_measurement_reader.py
@@ -1,45 +1,73 @@
-from source.measurement_reader import MeasurementReader
+from source.measurement_reader import MeasurementReader, get_results_aggregate
 from source.measurement_source import MeasurementSource
 import unittest
 from mock import MagicMock
-from datetime import timedelta
+from datetime import datetime, timedelta
+import pandas as pd
+
 
 class TestMeasureReader(unittest.TestCase):
 
 	def test_init(self):
-		mr = MeasurementReader(timedelta(seconds=10), 1)
+		mr = MeasurementReader(timedelta(seconds=60), timedelta(seconds=10), 1)
+		self.assertEqual(mr.idle_time, timedelta(seconds=60))
 		self.assertEqual(mr.duration, timedelta(seconds=10))
 		self.assertEqual(mr.number_of_measurements, 1)
 		
 	def test_add_source(self):
-		mr = MeasurementReader(timedelta(seconds=10), 1)
-		new_source = MeasurementSource("Device 1")
+		mr = MeasurementReader(timedelta(seconds=10), timedelta(seconds=10), 1)
+		new_source = MeasurementSource("Device 1", '*C')
 		mr.add_source(new_source)
 		self.assertEqual(len(mr.sources), 1)
 		self.assertIs(mr.sources["Device 1"], new_source)
 		self.assertEqual(mr.sources["Device 1"].device_unique_name, "Device 1")
 		
 	def test_get_measurements(self):
-		mr = MeasurementReader(timedelta(seconds=10), 1)
+		mr = MeasurementReader(timedelta(seconds=10), timedelta(seconds=10), 1)
 		
 		device_name = "Device 1"
-		measurement_to_be_returned = (20.0, 30.0)
-		def get_measurement_side_effect(arg):
-			arg[device_name] = measurement_to_be_returned
-			return arg
+		data = [['Device 1', 'temperature', '*C', 26.6], ['Device 1', 'humidity', '%', 33.3]]
+		measurement_to_be_returned = pd.DataFrame(data, columns=['device_name', 'measurement_type', 'measurement_unit', 'measurement_value'])
 		
 		# Create and mock the measurements device.
 		new_source = MeasurementSource(device_name)
-		new_source.get_measurement = MagicMock(side_effect=get_measurement_side_effect)
+		new_source.get_measurement = MagicMock(return_value=measurement_to_be_returned)
 		
 		mr.add_source(new_source)
 		
-		results_storage = {}
-		mr.get_measurements(results_storage)
+		res = mr.get_measurements()
 		
-		self.assertEqual(len(results_storage), 1)
-		self.assertEqual(results_storage[device_name], measurement_to_be_returned)
-		self.assertEqual(mr.sources[device_name].device_unique_name, device_name)
+		self.assertEqual(res.shape, (2, 4))
+		self.assertEqual(0, cmp(res.columns.tolist(), ['device_name', 'measurement_type', 'measurement_unit', 'measurement_value']))
 
+	def test_get_results_aggregate(self):
+		timestamp1 = datetime(2017, 12, 20, 21, 30, 10)
+		timestamp2 = datetime(2017, 12, 20, 21, 30, 20)
+		timestamp3 = datetime(2017, 12, 20, 21, 30, 50)
+		
+		data = [
+			[timestamp1, 'Device 1', 'temperature', '*C', 20.0],
+			[timestamp1, 'Device 1', 'humidity', '%', 30.0],
+			
+			[timestamp2, 'Device 1', 'temperature', '*C', 22.0],
+			[timestamp2, 'Device 1', 'humidity', '%', 35.0],
+			
+			[timestamp3, 'Device 1', 'temperature', '*C', 25.0],
+			[timestamp3, 'Device 1', 'humidity', '%', 40.0],
+			
+			[timestamp1, 'Device 2', 'temperature', '*C', 30.0],
+			[timestamp1, 'Device 2', 'humidity', '%', 50.0],
+			
+			[timestamp2, 'Device 2', 'temperature', '*C', 32.0],
+			[timestamp2, 'Device 2', 'humidity', '%', 55.0],
+			
+			[timestamp3, 'Device 2', 'temperature', '*C', 35.0],
+			[timestamp3, 'Device 2', 'humidity', '%', 60.0]
+			]
+		df = pd.DataFrame(data, columns=['timestamp', 'device_name', 'measurement_type', 'measurement_unit', 'measurement_value'])
+		res = get_results_aggregate(df)
+		
+		self.assertEqual(res.shape, (4, 5))
+		
 if __name__ == '__main__':
 	unittest.main()
