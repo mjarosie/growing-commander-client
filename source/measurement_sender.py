02eac928987c507a63b5cd7ae789ebaa83553d57
mjarosie <jarosiewicz.maciek@gmail.com>
2018-03-18 12:57:47 +0000

    make the class abstract using ABC

--- source/measurement_sender.py
+++ source/measurement_sender.py
@@ -1,4 +1,8 @@
-class MeasurementSender:
+from abc import ABC, abstractmethod
 
+
+class MeasurementSender(ABC):
+
+    @abstractmethod
     def send(self, data):
         raise NotImplementedError('Implement this!')
