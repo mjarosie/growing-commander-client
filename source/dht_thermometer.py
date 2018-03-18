f9a0dfe2962a5fe3c703c37ea4970bb5f130c7db
mjarosie <jarosiewicz.maciek@gmail.com>
2018-03-18 12:47:49 +0000

    reformat the code

--- source/dht_thermometer.py
+++ source/dht_thermometer.py
@@ -3,24 +3,42 @@ import Adafruit_DHT
 import pandas as pd
 from datetime import datetime
 
+
 class DhtThermometer(MeasurementSource):
-	def __init__(self, device_unique_name, gpio_pin):
-		self.device_unique_name = device_unique_name
-		self.measurement_types = ['humidity', 'temperature']
-		self.measurement_units = ['%', '*C']
-		self.gpio_pin = gpio_pin
-		self.sensor_type = Adafruit_DHT.AM2302
-		
-	def get_measurement(self, reading_function = None, *args, **kwargs):
-		# Try to grab a sensor reading.  Use the read_retry method which will retry up
-		# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-		timestamp = datetime.now()
-		if reading_function is None:
-			measurements = Adafruit_DHT.read_retry(self.sensor_type, self.gpio_pin)
-		else:
-			measurements = reading_function(*args, **kwargs)
-		
-		df = pd.DataFrame(columns=['timestamp', 'device_name', 'measurement_type', 'measurement_unit', 'measurement_value'])
-		df.loc[0] = [timestamp, self.device_unique_name, self.measurement_types[0], self.measurement_units[0], measurements[0]]
-		df.loc[1] = [timestamp, self.device_unique_name, self.measurement_types[1], self.measurement_units[1], measurements[1]]
-		return df
+    def __init__(self, device_unique_name, gpio_pin):
+        MeasurementSource.__init__(self, device_unique_name)
+        self.measurement_types = ['humidity', 'temperature']
+        self.measurement_units = ['%', '*C']
+        self.gpio_pin = gpio_pin
+        self.sensor_type = Adafruit_DHT.AM2302
+
+    def get_measurement(self, reading_function=None, *args, **kwargs):
+        # Try to grab a sensor reading.  Use the read_retry method which will retry up
+        # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+        timestamp = datetime.now()
+        if reading_function is None:
+            measurements = Adafruit_DHT.read_retry(self.sensor_type, self.gpio_pin)
+        else:
+            measurements = reading_function(*args, **kwargs)
+
+        df = pd.DataFrame(
+            columns=['timestamp', 'device_name', 'measurement_type', 'measurement_unit', 'measurement_value']
+        )
+
+        df.loc[0] = [
+            timestamp,
+            self.device_unique_name,
+            self.measurement_types[0],
+            self.measurement_units[0],
+            measurements[0]
+            ]
+
+        df.loc[1] = [
+            timestamp,
+            self.device_unique_name,
+            self.measurement_types[1],
+            self.measurement_units[1],
+            measurements[1]
+            ]
+
+        return df
